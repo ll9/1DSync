0890047a19efa52257a2c45dca04ded6189bfa38
ll9 <llwierer@web.de>
2019-02-17 19:54:10 +0100

    Cretaed db context

---
+++ 1DSync/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using _1DSync.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DSync.Data
+{
+    class ApplicationDbContext: DbContext
+    {
+        public DbSet<PseudoDynamicEntity> PseudoDynamicEntities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new SQLiteConnectionStringBuilder() { DataSource = "db.sqlite" };
+            optionsBuilder.UseSqlite(builder.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PseudoDynamicEntity>()
+                .Property(p => p.Id)
+                .HasDefaultValueSql("HEX(RANDOMBLOB(16))");
+        }
+    }
+}
